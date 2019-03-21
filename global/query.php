f03e2215fbb6152491922e3fa631907fa0e352da
Morplson <morplson@gmail.com>
2019-03-21 08:36:07 +0100

    new push

--- global/query.php
+++ global/query.php
@@ -32,95 +32,50 @@
 	} else {
 		//TODO: MySQL call
 	}
-	?>
-		<style type="text/css">
-			.scores>.like:hover {
-				color: #f91f1f;
-			}
-
-			.upvote:hover{
-				color: #5aa51d;
-			}
-
-			.downvote:hover{
-				color: #e52b06;
-			}
 
 
-			.scores>.comment:hover {
-				color: #9273d0;
-			}
+?>
 
-			.description {
-				text-align: justify;
-				padding: 0.125rem;
-				word-break: break-word;
-			}
 
-			.values.buy{
-			}
 
-			.buy .btn{
-				padding: 0;
-				background-color: transparent;
-				border: none;
-
-				height: 1.5rem;
-
-				width: 4rem;
-				float: left;
-			}
-
-			.buy .num{
-				padding: 0;
-				background-color: transparent;
-				border: none;
-
-				height: 1.5rem;
-
-				float: right;
-				width: 4rem;
-			}
-
-			.num:hover{
-				background-color: #f2f2f2;
-			}
-
-		</style>
-		<div class="container" id="<?php echo $pID; ?>">
-			<div class="values scores">
-				<div class="like">
-					<span class="fave-span" title="fave"><i class="fa fa-heart"></i></span>
-					<span class="favourites" title="Favourites"><?php echo $likes; ?></span>
-				</div>
-				<div class="vote">
-					<i class="upvote fa fa-arrow-up" title="Upvote"></i>
-					<span class="score" title="Score"><?php echo $score; ?></span>
-					<i class="downvote fa fa-arrow-down" title="Downvote"></i>
-				</div>
-				<div class="comment">
-					<i class="fa fa-comments"></i>
-					<span class="comments_count" data-image-id="1933082"><?php echo $comments; ?></span>
-				</div>
-			</div>
-
-			
-			<div class="values title">
-				<?php echo $title;?>
-			</div>
-
-			<picture style="background-image: url(<?php echo $imgLink; ?>);">
-				
-			</picture>
-
-			<div class="description">
-				<?php echo $description;?>
-				dfgs afjnbas jidfna snf löj dn fjna sk djfn osaj kdn fdfgs afjnbas jidfna snf löj dn fjna sk djfn osaj kdn fdfgs afjnbas jidfna snf löj dn fjna sk djfn osaj
-			</div>
-
-			<div class="values buy">
-				<button class="btn" id="<?php echo $pID; ?>">Kaufen</button>
-				<input class="num" id="<?php echo $pID; ?>" type="number" name="anzahl" min="1" max="<?php echo $anzahl; ?>" value="1">
-			</div>
-
+<div class="container" id="<?php echo $pID; ?>">
+	<div class="values scores">
+		<div id="<?php echo $pID; ?>likes" class="like" onClick="like('<?php echo $pID; ?>')">
+			<span class="fave-span" title="fave"><i class="fa fa-heart"></i></span>
+			<span class="favourites" title="Favourites">'.$likes.'</span>
 		</div>
+		<div id="<?php echo $pID; ?>votes" class="vote">
+			<i class="upvote fa fa-arrow-up" title="Upvote" onClick="vote('<?php echo $pID; ?>',1)"></i>
+			<span class="score" title="Score"><?php echo $score; ?></span>
+			<i class="downvote fa fa-arrow-down" title="Downvote" onClick="vote('<?php echo $pID; ?>',-1)"></i>
+		</div>
+		<div id="<?php echo $pID; ?>comments" class="comment" onClick="comment('<?php echo $pID; ?>')">
+			<i class="fa fa-comments"></i>
+			<span class="comments_count" data-image-id="'.$pID.'"> <?php echo $comments ?></span>
+		</div>
+	</div>
+	<div class="values title">
+		<?php echo $title; ?>
+	</div>
+
+	<a href="post.php?id=<?php echo $pID; ?>">
+		<picture style="background-image: url( <?php echo $imgLink; ?>);"></picture>
+	</a>
+	<div class="values gets">
+		<div class="preis">
+			Preis: <?php echo $preis; ?>
+		</div>
+
+		<div class="anzahl">
+			<?php echo $anzahl.' '.$einheit.''; ?>
+		</div>
+	</div>
+	<div class="description">
+		<?php echo $description; ?>
+	</div>
+
+	<div class="values buy">
+		<button class="btn" id="'.$pID.'button" onClick="buy(\''.$pID.'\')">Kaufen</button>
+		<input class="num" id="'.$pID.'anzahl" type="number" name="anzahl" min="1" max="'.$anzahl.'" value="1">
+	</div>
+</div>
